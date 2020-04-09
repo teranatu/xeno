064eb8daac9dba2bdbdfe61951e87479b58b3b64
teranatu <ranranheim@gmail.com>
2020-04-10 07:50:51 +0900

    xeno_V2.7 fixed500error

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -88,8 +88,7 @@ class GroupController extends Controller
      */
     public function update(Request $request, $id)
     {
-        if (DB::table('users')->where('group_id', '1')->count() <= 3) {
-            dd($id);
+        if (DB::table('users')->where('group_id', '1')->count() <= 4) {
             $group = new Group;
             $group->user_id = Auth::id();
             $group->save();
