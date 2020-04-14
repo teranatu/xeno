922ec1916c50689ccac8844cceddf651efd6b875
teranatu <ranranheim@gmail.com>
2020-04-15 02:14:58 +0900

    xeno_V5.5 fixed showCardDetails

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -28,10 +28,10 @@
 
             </div>
             <div class="col-3 text-center">
-              <button class="btn btn-primary pt-2 pb-2 w-80" onclick="style.display ='none';location.href='{{ route('drawCard')}}'">1枚引く</button>
+              <button class="btn btn-primary pt-2 pb-2 w-80" onclick="style.visibility ='hidden';location.href='{{ route('drawCard')}}'">1枚引く</button>
             </div>
             <div class="col-3 text-center">
-              <button class="btn btn-primary pt-2 pb-2 w-80" onclick="style.display ='none';location.href='{{ route('drawKillCard')}}'">転生札を引く</button>
+              <button class="btn btn-primary pt-2 pb-2 w-80" onclick="style.visibility ='hidden';location.href='{{ route('drawKillCard')}}'">転生札を引く</button>
             </div>
           </div>
         </div>
@@ -57,53 +57,61 @@
   <div style="height: 300px" class="ml-2 pr-2 mt-4 row">
 
     <div class="col-3">
-      <button class="btn btn-danger col-4 pt-2 pb-2" onclick="style.display ='none';location.href='{{ route('initialization')}}'">初期化</button>
-      <button class="btn btn-success col-2 pt-2 pb-2" onclick="style.display ='none';location.href='{{ route('selectCard')}}'">選択(7)効果使用</button>
-      <button class="btn btn-success col-2 pt-2 pb-2" onclick="style.display ='none';location.href='{{ route('exchangeCard')}}'">交換(8)効果使用</button>
-      @foreach($users as $user)
-      @if (isset($selectcard_1) && ($user->select_user == Auth::id()))
-      <div class="col-2">
-        <form method="POST" action="{{ route('selectedCard')}}">
-          @csrf
-          <button class="btn btn-primary pt-2 pb-2" type="submit">1枚目：{{ $selectcard_1 }}</button>
-          <input type="hidden" name="selectedCard" value="{{ $selectcard_1 }}">
-        </form>
+      <div class="row">
+        <div class="col-12">
+          <button class="btn btn-danger col-12 pt-2 pb-2" onclick="style.visibility ='hidden';location.href='{{ route('initialization')}}'">初期化</button>
+        </div>
+        <div class="col-12">
+          <button class="btn btn-success col-12 mt-2 pt-2 pb-2" onclick="style.visibility ='hidden';location.href='{{ route('selectCard')}}'">選択(7)効果使用</button>
+          <button class="btn btn-success col-12 mt-2 pt-2 pb-2" onclick="style.visibility ='hidden';location.href='{{ route('exchangeCard')}}'">交換(8)効果使用</button>
+        </div>
+        <div class="col-12">
+          @foreach($users as $user)
+          @if (isset($selectcard_1) && ($user->select_user == Auth::id()))
+          <div class="col-4">
+            <form method="POST" action="{{ route('selectedCard')}}">
+              @csrf
+              <button class="btn btn-primary pt-2 pb-2" type="submit">1枚目：{{ $selectcard_1 }}</button>
+              <input type="hidden" name="selectedCard" value="{{ $selectcard_1 }}">
+            </form>
+          </div>
+          @endif
+            @if (isset($selectcard_2) && ($user->select_user == Auth::id()))
+          <div class="col-4">
+            <form method="POST" action="{{ route('selectedCard')}}">
+              @csrf
+              <button class="btn btn-primary pt-2 pb-2" type="submit">2枚目：{{ $selectcard_2 }}</button>
+              <input type="hidden" name="selectedCard" value="{{ $selectcard_2 }}">
+            </form>
+          </div>
+          @endif
+            @if (isset($selectcard_3) && ($user->select_user == Auth::id()))
+          <div class="col-4">
+            <form method="POST" action="{{ route('selectedCard')}}">
+              @csrf
+              <button class="btn btn-primary pt-2 pb-2" type="submit">3枚目：{{ $selectcard_3 }}</button>
+              <input type="hidden" name="selectedCard" value="{{ $selectcard_3 }}">
+            </form>
+          </div>
+          @endif
+          @if ((($user->id != Auth::id())) && ($user->exchange_user == Auth::id()))
+          <div class="col-4">
+            <form method="POST" action="{{ route('exchangedCard')}}">
+              @csrf
+              <button class="btn btn-primary pt-2 pb-2" type="submit">{{ $user->name }}</button>
+              <input type="hidden" name="targetName" value="{{ $user->name }}">
+            </form>
+          </div>
+          @endif
+          @endforeach
+        </div>
       </div>
-      @endif
-        @if (isset($selectcard_2) && ($user->select_user == Auth::id()))
-      <div class="col-2">
-        <form method="POST" action="{{ route('selectedCard')}}">
-          @csrf
-          <button class="btn btn-primary pt-2 pb-2" type="submit">2枚目：{{ $selectcard_2 }}</button>
-          <input type="hidden" name="selectedCard" value="{{ $selectcard_2 }}">
-        </form>
-      </div>
-      @endif
-        @if (isset($selectcard_3) && ($user->select_user == Auth::id()))
-      <div class="col-2">
-        <form method="POST" action="{{ route('selectedCard')}}">
-          @csrf
-          <button class="btn btn-primary pt-2 pb-2" type="submit">3枚目：{{ $selectcard_3 }}</button>
-          <input type="hidden" name="selectedCard" value="{{ $selectcard_3 }}">
-        </form>
-      </div>
-      @endif
-      @if ((($user->id != Auth::id())) && ($user->exchange_user == Auth::id()))
-      <div class="col-2">
-        <form method="POST" action="{{ route('exchangedCard')}}">
-          @csrf
-          <button class="btn btn-primary pt-2 pb-2" type="submit">{{ $user->name }}</button>
-          <input type="hidden" name="targetName" value="{{ $user->name }}">
-        </form>
-      </div>
-      @endif
-      @endforeach
     </div>
     
     <div class="col-6">
       <div class="row">
         <div class="col-6 text-center">
-          <button class="btn btn-success w-50" onclick="style.display ='none';location.href='{{ route('discardLeft')}}'">左を使用</button>
+          <button class="btn btn-success w-50" onclick="style.visibility ='hidden';location.href='{{ route('discardLeft')}}'">左を使用</button>
           @for ( $i = 1;$i < 11;$i++ )
             @if ( $i == Auth::user()->card_1 )
               <img class="w-75 mt-4" src="{{ asset("/xenoCards/xenoCard_$i.png") }}">
@@ -112,7 +120,7 @@
           
         </div>
         <div class="col-6 text-center">
-          <button class="btn btn-success w-50" onclick="style.display ='none';location.href='{{ route('discardRight')}}'">右を使用</button>
+          <button class="btn btn-success w-50" onclick="style.visbility ='hidden';location.href='{{ route('discardRight')}}'">右を使用</button>
           @for ( $i = 1;$i < 11;$i++ )
             @if ( $i == Auth::user()->card_2 )
               <img class="w-75 mt-4" src="{{ asset("/xenoCards/xenoCard_$i.png") }}">
