8ba41d0bc8a3c8df1ff6af70017c814f5a0b1c4d
teranatu <ggwpexitdesktop@gmail.com>
2020-05-02 10:01:24 +0900

    xeno_V9.14 fixed groups test4

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,7 +12,7 @@ class CreateUsersTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {//
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
