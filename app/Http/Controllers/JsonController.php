cc8f8179dea243a3be2ec4c02b8c3fa919e2c961
teranatu <ggwpexitdesktop@gmail.com>
2020-05-02 17:16:38 +0900

    xeno_V9.4 add dd delete

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -64,7 +64,6 @@ class JsonController extends Controller
         $inRoomUsersDetails = [];
         $inRoomPublicexectute = [];
 
-        dd(User::where('group_id',1)->get());
         for ($i=1,$ii=1; $i < 11 ; $i++,$ii += 10) {
             ${'inRoomUsersDetails_'.$i} = User::where('group_id',$ii)->orderBy('group_number', 'asc')->get();
             $inRoomUsersDetails[] = ${'inRoomUsersDetails_'.$i};
