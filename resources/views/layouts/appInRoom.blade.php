8300f86424094d7889f85d8e7f1ec065aac3db53
teranatu <ggwpexitdesktop@gmail.com>
2020-05-02 14:42:09 +0900

    xeno_V9.16 fixed groups test6

--- resources/views/layouts/appInRoom.blade.php
+++ resources/views/layouts/appInRoom.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Cache-Control" content="no-cache">
+    <meta http-equiv="Pragma" content="no-cache">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
