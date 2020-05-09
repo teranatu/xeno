7bb10e6c373b13403108bb232d78187fb2e91479
teranatu <ggwpexitdesktop@gmail.com>
2020-05-09 21:19:47 +0900

    xeno_V10.4.6 https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceScheme('https');
         Schema::defaultStringLength(191);
     }
 }
