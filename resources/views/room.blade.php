3f215724eaa2d532bc960de2ecf656e3fdaba840
teranatu <ggwpexitdesktop@gmail.com>
2020-05-09 18:27:09 +0900

    xeno_V10.2 fixed UI height

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -4,7 +4,7 @@
 
   <div class="ml-2 pr-2 row">
 
-    <div style="height: 300px" class="col-3">
+    <div style="height: 350px" class="col-3">
       <div class="card col-12 h-15">
         <div class="row h-100">
           <div class="col-4 d-flex align-items-center">ユーザー</div>
@@ -16,7 +16,7 @@
       </div>
     </div>
 
-    <div style="height: 300px" class="col-6">
+    <div style="height: 350px" class="col-6">
         <div class="card h-100">
           <div class="row">
             <div id="isCountCard" class="col-6 pt-2 pb-2 text-center">0</div>
@@ -37,7 +37,7 @@
         </div>
     </div>
 
-    <div style="height: 300px" class="col-3 card">
+    <div style="height: 350px" class="col-3 card">
       <div class="row h-100">
         <div class="col-6 d-flex align-items-center" id="Deadcard_1">1:0枚</div>
         <div class="col-6 d-flex align-items-center" id="Deadcard_2">2:0枚</div>
